39e6ad38170128511a23591da9944b1dcc5ad6c9
Christian Haggerty <captivationstation@gmail.com>
2022-01-31 09:41:00 -0600

    added comments to all functions

--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -11,13 +11,24 @@ import { bizzy } from "./Business.js"
 
 const contentTarget = document.querySelector("#container")
 
-// declaring and exporting a function with no parameter
+// declaring and exporting a function with no parameter 
+
+
 export const bizList = () => {
 
+    // declaring a variable whos value is a copy of the database array 
     const bizArray = getBizzy()
 
+    // what is content target? 
+    // i know this is adding an html string to my inner html 
+    // on indenx.html
     contentTarget.innerHTML = "<h1>Active BizzieZ</h1>"
 
+        //  iterating my biz array from the database
+        // using the for each method taking a bizObject
+        // as a parameter and invoking my bizzy function 
+        // with the biz object as its parameter
+        // then adding each buisiness and its info to the innerHTML
     bizArray.forEach(
         (bizyObject) => {
             contentTarget.innerHTML += bizzy(bizyObject)
--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,8 @@ import { bizzy } from "./Business.js"
 import { bizList } from "./BusinessList.js"
 import { getBizzy } from "./database.js"
 
+// Invoking my bizlist function that will display all of my inner html 
+// in the web browser
 
 bizList()
 
